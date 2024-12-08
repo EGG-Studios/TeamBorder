1a94d1af8408aafae851d0644836b2db3d06d899
Fabian <105858467+Foxiel@users.noreply.github.com>
2024-12-08 22:43:19 +0100

    Initial commit

---
+++ src/main/kotlin/org/croxie/teamBorder/TeamBorder.kt
@@ -0,0 +1,14 @@
+package org.croxie.teamBorder
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TeamBorder : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
