181a1514fdeda1857df455cfd0ed15d2209c747f
Fabian <105858467+Foxiel@users.noreply.github.com>
2025-04-22 17:23:44 +0200

    Damage is synced within teams

--- src/main/kotlin/org/croxie/teamBorder/TeamBorder.kt
+++ src/main/kotlin/org/croxie/teamBorder/TeamBorder.kt
@@ -1,12 +1,19 @@
 package org.croxie.teamBorder
 
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.Listener
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
 
-class TeamBorder : JavaPlugin() {
+class TeamBorder : JavaPlugin(), Listener {
+    private val damagedPlayers = mutableSetOf<UUID>()
 
     override fun onEnable() {
+        server.pluginManager.registerEvents(this, this)
         saveDefaultConfig()
 
         val worldBorderManager = WorldBorderManager(this)
@@ -28,10 +35,29 @@ class TeamBorder : JavaPlugin() {
                 val team = teamManager.createTeam()
 
                 teamManager.addPlayer(player, team)
-                player.sendMessage("You have been added to the team: $team")
+                player.sendMessage("You have been added to: $team")
             } else {
                 teamManager.addPlayer(player, teamName)
-                player.sendMessage("You have been added to the team: $teamName")
+                player.sendMessage("You have been added to: $teamName")
+            }
+        }
+    }
+
+    @EventHandler
+    fun onEntityDamage(event: EntityDamageEvent) {
+        if (event.entity is Player) {
+            val player = event.entity as Player
+            if (damagedPlayers.contains(player.uniqueId)) return
+
+            val team = PlayerManager().checkTeam(player)
+            val onlineTeammates = team?.entries
+                ?.mapNotNull { Bukkit.getPlayer(it) }
+                ?.filter { it.uniqueId != player.uniqueId }
+
+            onlineTeammates?.forEach { teammate ->
+                damagedPlayers.add(teammate.uniqueId)
+                teammate.damage(event.damage)
+                damagedPlayers.remove(teammate.uniqueId)
             }
         }
     }
